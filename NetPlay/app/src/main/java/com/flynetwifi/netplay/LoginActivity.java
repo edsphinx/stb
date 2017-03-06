311a9f664aceb684ab533fc229d38511ff821125
flynet <alerts@panoramalife.com>
2017-03-06 11:54:15 -0600

    Update Automatic Login

--- NetPlay/app/src/main/java/com/flynetwifi/netplay/LoginActivity.java
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/LoginActivity.java
@@ -15,6 +15,8 @@ import com.flynetwifi.netplay.Tasks.LoginTask;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -128,7 +130,7 @@ public class LoginActivity extends Activity {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                String url = "http://10.115.0.3:8080/stb/cuenta/login/D0:76:58:06:EC:39";
+                String url = "http://10.115.0.3:8080/stb/cuenta/login/" + getMacAddress();
                 String bearer = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjRhYzdjYjc1MzU5NjAwNTk3ZTg5NzQzZjFlNzFkZjc3NmEzZjNmZTM4NGMxODllNWE5NWFlMWZiZDg5ZTY5MjYyNzRmYzAxYTU1MzEwOTVkIn0.eyJhdWQiOiI3IiwianRpIjoiNGFjN2NiNzUzNTk2MDA1OTdlODk3NDNmMWU3MWRmNzc2YTNmM2ZlMzg0YzE4OWU1YTk1YWUxZmJkODllNjkyNjI3NGZjMDFhNTUzMTA5NWQiLCJpYXQiOjE0ODg1ODA2MzQsIm5iZiI6MTQ4ODU4MDYzNCwiZXhwIjoxNTIwMTE2NjM0LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.b5JNXeJD1UgV42ygq6WyjsAwKckbpsQDq1U2o5ckeT4QguRvo0yXNJ5DGwXPUvxyhGMHrIs1zxVm2k8tvOzYKhL8l4ahHAMS_c4G1rAemPdOA-z_tlZc37LwGKRb6mJj0_XI0K-GCNWibeLmjmxKXvRIzhgOj0Im6mde4HUGTkMeGJb50IQIQlzZnRDGmOlFghQL62OqDCFyR6BxsU1E1xUyuxnh0rxK5jiOw56JACJ3ylcB0vO0Hs0g56gq0as_Ic_VetdaqWynGJMX5TZz-stHxl_r4b4McWcAg4Uj2klsjSrF86qb4GjoXRDaOmBGCpDHhArKKvu9QzNkr-s95SuG8JGwwOrFPkSuhM6JHZ6RRt-iEPqhH-OiTWTsB5Pn06ce7FURVJ_scJVgjwVGxXJ4k69qSBuLXQEj-AFHRKWyR_ept2kPiyb1lQ2jJEygn_y5n2mHeNFVMEad_V0QeZ1eqrm-Gs33iIKmgS8TlIKlPLExhs_-6IZrtH2-DWfXhpuqSXwOBwkpxAYNipJXDVwTOfNafSgqLxqNPpd5Mv6tmGuOZwzCiP1YRIOifjgnc0eW6LSHW3s312IgOyovIWiXdHix6lc_j3A3ttKiAT5KRhnWB1wWT20ldUe1QQ5RvY3w-CI-fJPZA31PO7RRCgvGedEFNqwCLcSnsV1QJVs";
 
                 final OkHttpClient client = new OkHttpClient();
@@ -176,5 +178,33 @@ public class LoginActivity extends Activity {
         }
 
 
+
+
+
     }
+
+
+    public String getMacAddress() {
+        try {
+            return loadFileAsString("/sys/class/net/eth0/address")
+                    .toUpperCase().substring(0, 17);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String loadFileAsString(String filePath) throws java.io.IOException {
+        StringBuffer fileData = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+    }
+
 }
