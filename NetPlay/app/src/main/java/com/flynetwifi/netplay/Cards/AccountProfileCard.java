c9a88f1230b5fdd18d0585d1b9e02d3894a82d68
flynet <alerts@panoramalife.com>
2017-03-14 10:20:56 -0600

    Add Interface field

--- NetPlay/app/src/main/java/com/flynetwifi/netplay/Cards/AccountProfileCard.java
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/Cards/AccountProfileCard.java
@@ -8,6 +8,7 @@ public class AccountProfileCard {
     @SerializedName("nombre") private String mTitle = "";
     @SerializedName("servicio") private String mService = "";
     @SerializedName("control") private int mParentalControl = 1;
+    @SerializedName("interface") private int mInterface = 0;
 
     public Integer getmId() {
         return mId;
@@ -29,4 +30,23 @@ public class AccountProfileCard {
         return mParentalControl;
     }
 
+    public String getmService() {
+        return mService;
+    }
+
+    public void setmService(String mService) {
+        this.mService = mService;
+    }
+
+    public void setmParentalControl(int mParentalControl) {
+        this.mParentalControl = mParentalControl;
+    }
+
+    public int getmInterface() {
+        return mInterface;
+    }
+
+    public void setmInterface(int mInterface) {
+        this.mInterface = mInterface;
+    }
 }
