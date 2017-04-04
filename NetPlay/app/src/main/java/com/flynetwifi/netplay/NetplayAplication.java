1cf81d8badf58e63ddfa640bdbc4a2c09e04f0e7
Oscar Fonseca <ofonseca@panoramalife.com>
2017-04-04 11:28:59 -0600

    fix to missing classes

---
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/NetplayAplication.java
@@ -0,0 +1,8 @@
+package com.flynetwifi.netplay.Utils;
+
+/**
+ * Created by fonseca on 4/4/17.
+ */
+
+public class NetplayAplication {
+}
--- NetPlay/app/src/main/java/com/flynetwifi/netplay/Rows/LiveCanalRow.java
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/Rows/LiveCanalRow.java
@@ -1,7 +1,7 @@
 package com.flynetwifi.netplay.Rows;
 
 /**
- * Created by fonseca on 3/29/17.
+ * Created by fonseca on 4/4/17.
  */
 
 public class LiveCanalRow {
