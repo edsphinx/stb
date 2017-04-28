8424d21a0898c93a63a24ef6a59e8f141d8ec0e2
Oscar Fonseca <ofonseca@panoramalife.com>
2017-04-27 21:58:01 -0600

    exoplayer movies added with subtitles loading

---
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/Requests/DownloadAPK.java
@@ -0,0 +1,8 @@
+package com.flynetwifi.netplay.Requests;
+
+/**
+ * Created by fonseca on 4/26/17.
+ */
+
+public class DownloadAPK {
+}
---
+++ NetPlay/app/src/main/java/com/flynetwifi/netplay/UpdateActivity.java
@@ -0,0 +1,8 @@
+package com.flynetwifi.netplay;
+
+/**
+ * Created by fonseca on 4/26/17.
+ */
+
+public class UpdateActivity {
+}
